d99c41921b43c0435475d72b6a864bf23edf2bbc
Muhammad Eman <123630981+muhammademanabbas@users.noreply.github.com>
2024-01-12 00:27:01 +0500

    Update script.js

--- script.js
+++ script.js
@@ -14,6 +14,19 @@ form.addEventListener("submit", (e) => {
   container.forEach((v, i) => {
     subject_weight_average[i] = v.childNodes[3].value * v.childNodes[5].value;
     subject_credit_hour[i] = v.childNodes[5].value * 1;
+    if(v.childNodes[3].value <= 4  && v.childNodes[3].value  > 3.9 ) {
+      v.childNodes[7].value = "A";
+    }
+    else if(v.childNodes[3].value <= 3.9  && v.childNodes[3].value  >= 3 ) {
+      v.childNodes[7].value = "B";
+    }
+    else if(v.childNodes[3].value >= 2.0   && v.childNodes[3].value  <= 2.9 ) {
+      v.childNodes[7].value = "C";
+    }
+    else if (v.childNodes[3].value  <=2.9 && v.childNodes[3].value  > 0 ) {
+      v.childNodes[7].value = "F";
+    }
+    
   });
   total_weight_average_CGPA = subject_weight_average.reduce((acc, curr) => {
     acc = acc + curr;
@@ -27,6 +40,11 @@ form.addEventListener("submit", (e) => {
   GPA = GPA.toFixed(2);
   average = (GPA * 100) / 4;
   average = average.toFixed(2);
-  result.innerHTML = "GPA is = " + GPA;
-  averageMarks.innerHTML = "Average is = " + average + "%";
+  if (isNaN(average) || isNaN(GPA)) {
+    result.innerHTML = "Please enter valid marks!";
+    averageMarks.innerHTML = "";
+  }else{
+    result.innerHTML = "GPA is = " + GPA;
+    averageMarks.innerHTML = "Average is = " + average + "%";
+  }
 });
