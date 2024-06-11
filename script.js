78c45af3339dfc24ffbc24de6a95aa489c08da03
Muhammad Eman <123630981+muhammademanabbas@users.noreply.github.com>
2024-06-11 21:38:26 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,94 +1,50 @@
-let definition = document.querySelector("#result");
-let searchWord = document.querySelector("#search");
-let searchBtn = document.querySelector(".search-btn");
-let AllSearches = document.querySelector("#searches");
-let deleteAllSearchesBtn = document.querySelector(".ri-delete-bin-6-line");
-let searchedWord ;
+let container = document.querySelectorAll(".container");
+let result = document.querySelector("#result");
+let averageMarks = document.querySelector("#averageMarks");
+let form = document.querySelector(".input-form");
+let GPA = 0;
+let average = 0;
+let total_credit_hours = 0;
+let total_weight_average_CGPA = 0;
+let subject_weight_average = [];
+let subject_credit_hour = [];
 
-displaySearchHistory()
-
-function getSeacrhResult() {
-    searchBtn.addEventListener("click", (e) => {
-        
-        e.preventDefault();
-        const searchTerm = searchWord.value.trim(); // Trim leading/trailing spaces
-
-        if (searchTerm) {
-            fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${searchTerm}`)
-                .then((response) => response.json())
-                .then((data) => {
-                    if (data.length) {
-                        const definitionText =
-                            data[0].meanings[0].definitions[0].definition;
-                        definition.innerText = definitionText;
-                        definition.style.textDecoration = "underline";
-                        let prevLocalData = JSON.parse(localStorage.getItem('searchHistory')) || [];
-                        prevLocalData.push(searchWord.value);
-                        localStorage.setItem('searchHistory', JSON.stringify(prevLocalData));
-                        displaySearchHistory();
-
-                    } else {
-                        definition.innerText = "No definition found.";
-                        definition.style.textDecoration = "underline";
-                    }
-                })
-                .catch((error) => {
-                    // Handle network or API errors
-                    console.error("Error fetching definition:", error);
-                    definition.innerText = "An error occurred. Please try again later.";
-                });
-        } else {
-            window.alert("Please Enter a Word to Search");
-        }
-    });
-}
-getSeacrhResult();
-
-function searchHistoryTab() {
-    document.querySelector(".search-history").addEventListener("click", (e) => {
-        document.querySelector(".check-search-history").style.transform =
-            "translateX(0%)";
-    });
-    document
-        .querySelector(".ri-close-large-line")
-        .addEventListener("click", (e) => {
-            document.querySelector(".check-search-history").style.transform =
-                "translateX(100%)";
-        });
-        document.querySelector('#landing-page').addEventListener("click", (e) => {
-            document.querySelector(".check-search-history").style.transform =
-                "translateX(100%)";
-        });
-}
-searchHistoryTab();
-
-function displaySearchHistory() {
-    let localStorageData = JSON.parse(localStorage.getItem('searchHistory')) || [];
-    if(localStorageData.length === 0){
-        AllSearches.innerHTML = `<p class="no-search">No Search History</p>`;
-        return ;
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  container.forEach((v, i) => {
+    subject_weight_average[i] = v.childNodes[3].value * v.childNodes[5].value;
+    subject_credit_hour[i] = v.childNodes[5].value * 1;
+    if(v.childNodes[3].value <= 4  && v.childNodes[3].value  > 3.9 ) {
+      v.childNodes[7].value = "A";
     }
-    let historyWords = `` ; 
-     for(let i = 0 ;   i<localStorageData.length ; i ++){
-        historyWords+= `<div>
-          <p class="searched">${localStorageData[i]}</p>
-        </div>`
-        // <p class="clear-one-search">Clear</p>
-     }
-     console.log(historyWords);
-    AllSearches.innerHTML  =  historyWords ;
-}
-
-function deleteAllSearchHistory(){
-    deleteAllSearchesBtn.addEventListener("click", (e) => {
-        // localStorage.clear();
-        for (let i = 0; i < localStorage.length; i++) {
-            const key = localStorage.key(i);
-            localStorage.removeItem(key);
-          }
-
-        displaySearchHistory();
-    });
+    else if(v.childNodes[3].value <= 3.9  && v.childNodes[3].value  >= 3 ) {
+      v.childNodes[7].value = "B";
+    }
+    else if(v.childNodes[3].value >= 2.0   && v.childNodes[3].value  <= 2.9 ) {
+      v.childNodes[7].value = "C";
+    }
+    else if (v.childNodes[3].value  <=2.9 && v.childNodes[3].value  > 0 ) {
+      v.childNodes[7].value = "F";
+    }
     
-}
-deleteAllSearchHistory()
+  });
+  total_weight_average_CGPA = subject_weight_average.reduce((acc, curr) => {
+    acc = acc + curr;
+    return acc;
+  });
+  total_credit_hour = subject_credit_hour.reduce((acc, curr) => {
+    acc = acc + parseInt(curr);
+    return acc;
+  });
+  GPA = total_weight_average_CGPA / total_credit_hour;
+  GPA = GPA.toFixed(2);
+  average = (GPA * 100) / 4;
+  average = average.toFixed(2);
+  if (isNaN(average) || isNaN(GPA)) {
+    result.innerHTML = "Please enter valid marks!";
+    averageMarks.innerHTML = "";
+  }else{
+    result.innerHTML = "GPA is = " + GPA;
+    averageMarks.innerHTML = "Average is = " + average + "%";
+  }
+});
